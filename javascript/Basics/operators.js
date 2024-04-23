00debed234afb3aeca68758e389b667f985b6730
Farhann209 <farhan23420@gmail.com>
2024-04-23 08:53:37 +0545

    random number task completed

---
+++ javascript/Basics/operators.js
@@ -0,0 +1,26 @@
+//Q1 create two constants, add them assign to a new constants
+const x = 40
+const y = 20
+const z = (x + y)
+console.log(z)
+
+
+//Check below code, output should be false by comparing. console.log the answer
+const a = 10
+const b = '10'
+
+console.log(a===b)
+
+
+// generate a random number between 0 and 5, if it is lesser than 3,
+//console.log "lesser than 3" else "Greater"
+const randomNum = Math.random()*6
+const wholeNum = Math.floor(randomNum)
+console.log(wholeNum)
+
+if (wholeNum<3){
+console.log("Number is less than 3")
+}
+else{
+    console.log("Number is 3 or greater.")
+}
