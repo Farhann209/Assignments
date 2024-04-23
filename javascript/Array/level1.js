c60deb6de23af3ecd17cc4a87293da7a4e2d7ab5
Farhann209 <farhan23420@gmail.com>
2024-04-23 09:14:19 +0545

    array task 1 completed

---
+++ javascript/Array/level1.js
@@ -0,0 +1,17 @@
+const arr = [3,5,2]
+//Q1 calculate length of array and console log
+//expected output is 3
+console.log(arr)
+console.log(arr.length)
+
+
+
+
+//Q2 reverse the array and console log
+//expected output [2,5,3]
+const arrr = [...arr]
+console.log(arrr.reverse())
+
+///Q3 join the array and console log
+// expected output 352
+console.log(arr.join(""))
