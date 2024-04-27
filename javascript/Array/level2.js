b624ba8bbbc7e7b173c020176e4de6e3d81c667e
Farhann209 <farhan23420@gmail.com>
2024-04-27 11:29:38 +0545

    Q3 done and uploaded Q4 still left

--- javascript/Array/level2.js
+++ javascript/Array/level2.js
@@ -1,3 +1,24 @@
-const arr =  [3,5,2]
-//console.log the maximum number of an array
+//Q1. console.log the maximum number of an array
 //expectedd output 5
+const arr =  [3,5,2]
+const maxNum = Math.max(...arr)
+console.log(maxNum)
+
+//Q2 Remove a specific element 2 from the array. Log the modified array to the console.
+// expected output: [3,5]
+const removeArr = arr.slice(0,2)
+console.log(removeArr)
+
+
+
+//Q3 Multiply each element of the array by a constant value 5 and log the modified array to the console.
+//expected output [15,10]
+arr.splice(1,1)
+const multipliedArr = arr.map((item)=>{
+    return item * 5
+})
+console.log(multipliedArr)
+
+//find intersection (common elements) of arr and arr2
+//expected output: [5]
+const arr2 = [5,21,32]
