3c7f53eb08a1975393e81fe51e4d316e08a011d6
Farhann209 <farhan23420@gmail.com>
2024-04-29 17:30:02 +0545

    sum of odd number done

--- javascript/Array/level3.js
+++ javascript/Array/level3.js
@@ -1,10 +1,10 @@
 //given array, calculate sum of odd numbers using reduce method
 const arr = [4, 2, 3, 5]
 
-arr.reduce((sum, item)=>{
-    if(item%2 in arr != 0){
+const sumOdd = arr.reduce((sum, item)=>{
+    if(item%2 != 0){
         sum = sum + item
     }
     return sum
 }, 0)
-console.log()
+console.log(sumOdd)
